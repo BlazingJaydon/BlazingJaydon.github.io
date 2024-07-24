320c6c701731b4d797512f77c5344f365cc8f832
Jaydon Hodge <jaydonhodge@gmail.com>
2024-07-24 12:27:33 -0400

    made spacing adjusts

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const navMenu = document.getElementById('nav-menu'),
-    toggleMenu = document.getElementById('toggle-menu'),
-    borderBar = document.getElementById('border-bar');
+        toggleMenu = document.getElementById('toggle-menu'),
+        resumeButton=document.getElementById('resume'),
+        logoAnch=document.getElementById('logo-anch'),
+        borderBar = document.getElementById('border-bar');
+
+var navToggles=document.getElementsByClassName('page-links');
 
 
 toggleMenu.addEventListener('click',() => {
@@ -8,3 +12,22 @@ toggleMenu.addEventListener('click',() => {
     borderBar.classList.toggle('show');
 
 })
+
+resumeButton.addEventListener('click',() => {
+    navMenu.classList.toggle('show');
+    borderBar.classList.toggle('show');
+})
+
+logoAnch.addEventListener('click',() => {
+    if (navMenu.classList.contains('show')) {
+        navMenu.classList.toggle('show');
+        borderBar.classList.toggle('show');
+    }
+})
+
+Array.prototype.forEach.call(navToggles, function(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+        borderBar.classList.toggle('show');
+    });
+});
